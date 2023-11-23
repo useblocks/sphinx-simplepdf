9faded6f58bf55f73f8d9ae6813e083c55951b45
Ad Thiers <a.thiers@atcomputing.nl>
2023-11-23 13:22:49 +0100

    Put docstring back in get_config_var and get_theme_var

--- sphinx_simplepdf/themes/simplepdf_theme/__init__.py
+++ sphinx_simplepdf/themes/simplepdf_theme/__init__.py
@@ -18,11 +18,36 @@ def get_html_theme_path():
 
 def create_custom_css(app):
 
+    try:
+        simplepdf_vars = app.config.simplepdf_vars
+    except AttributeError:
+        simplepdf_vars = {}
+
     def get_config_var(name, default):
-        return app.config.simplepdf_vars.get(name, default)
+        """
+        Gets a config variables for scss out of the Sphinx configuration.
+        If name is not found in config, the specified default var is returned.
+
+        Args:
+            name: Name of the config var to use
+            default: Default value, if name can not be found in config
+
+        Returns: Value
+        """
+        return simplepdf_vars.get(name, default)
 
     def get_theme_var(name, default):
-        return app.config.simplepdf_theme_options.get(name, default)
+        """
+        Gets a option  variables for scss out of the Sphinx theme options.
+        If name is not found in theme options, the specified default var is returned.
+
+        Args:
+            name: Name of the option var to use
+            default: Default value, if name can not be found in config
+
+        Returns: Value
+        """
+        return app.config.html_theme_options.get(name, default)
 
     here = path.abspath(path.dirname(__file__))
     scss_folder = path.join(here, "static", "styles", "sources")
